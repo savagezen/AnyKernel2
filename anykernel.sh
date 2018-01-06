1b06c8e938159baaf670066b6cb9a79153474f95
Austin Haedicke <austin.haedicke@gmail.com>
2018-01-06 13:48:20 -0500

    base changes to anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,23 +4,21 @@
 ## AnyKernel setup
 # begin properties
 properties() {
-kernel.string=
+kernel.string=Nexus 6P Angler kernel by @savagezen
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=
-device.name2=
+device.name1=angler
+device.name2=Angler
 device.name3=
 device.name4=
 device.name5=
 } # end properties
 
 # shell variables
-block=;
+block=/dev/block/platform/soc.0/f9824900.sdhci/by-name/boot;
 is_slot_device=0;
-ramdisk_compression=auto;
-
 
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
