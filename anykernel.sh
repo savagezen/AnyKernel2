6bdfc7c8839610f86ce99927c585b389c4c185a5
Austin Haedicke <austin.haedicke@gmail.com>
2018-11-18 20:03:39 -0500

    rebase to franco kernel r76 to test working ramdisk changes

--- anykernel.sh
+++ anykernel.sh
@@ -1,22 +1,16 @@
 # AnyKernel2 Ramdisk Mod Script
 # osm0sis @ xda-developers
-# parts taken from Flash Kenrel by @nathanchance
-# parts taken from Fraco Kernel by @franciscofranco
 
 ## AnyKernel setup
 # begin properties
-properties() { '
+properties() {
 kernel.string=Nexus 6P Angler Kernel
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
 do.cleanuponabort=1
 device.name1=angler
-device.name2=Angler
-device.name3=
-device.name4=
-device.name5=
-'; } # end properties
+} # end properties
 
 # shell variables
 block=/dev/block/platform/soc.0/f9824900.sdhci/by-name/boot;
@@ -38,7 +32,6 @@ case "$android_ver" in
   "7.0"|"7.1"|"7.1.1"|"7.1.2"|"8.0.0") compatibility_string="your version is unsupported, expect no support!";;
   "8.1.0") compatibility_string="your version is supported!";;
 esac;
-ui_print "Running Android $android_ver, $compatibility_string";
 
 ## AnyKernel install
 dump_boot;
@@ -46,29 +39,27 @@ dump_boot;
 # begin ramdisk changes
 
 # fstab.angler
-insert_line fstab.angler "data           f2fs" after "data           ext4" "/dev/block/platform/soc.0/f9824900.sdhci/by-name/userdata     /data           
-f2fs    rw,nosuid,nodev,noatime,nodiratime,inline_xattr 
-wait,formattable,encryptable=/dev/block/platform/soc.0/f9824900.sdhci/by-name/metadata";
-insert_line fstab.angler "cache          f2fs" after "cache          ext4" "/dev/block/platform/soc.0/f9824900.sdhci/by-name/cache        
-/cache          f2fs    rw,nosuid,nodev,noatime,nodiratime,inline_xattr wait,check,formattable";
+insert_line fstab.angler "data           f2fs" after "data           ext4" "/dev/block/platform/soc.0/f9824900.sdhci/by-name/userdata     /data           f2fs    rw,nosuid,nodev,noatime,nodiratime,inline_xattr wait,formattable,encryptable=/dev/block/platform/soc.0/f9824900.sdhci/by-name/metadata";
+insert_line fstab.angler "cache          f2fs" after "cache          ext4" "/dev/block/platform/soc.0/f9824900.sdhci/by-name/cache        /cache          f2fs    rw,nosuid,nodev,noatime,nodiratime,inline_xattr wait,check,formattable";
 patch_fstab fstab.angler none swap flags "zramsize=533413200" "zramsize=1066826400";
 patch_fstab fstab.angler /system ext4 flags "wait,verify=/dev/block/platform/soc.0/f9824900.sdhci/by-name/metadata" "wait";
 patch_fstab fstab.angler /vendor ext4 flags "wait,verify=/dev/block/platform/soc.0/f9824900.sdhci/by-name/metadata" "wait";
 patch_fstab fstab.angler /data ext4 flags "wait,check,forcefdeorfbe=/dev/block/platform/soc.0/f9824900.sdhci/by-name/metadata" "wait,check,encryptable=/dev/block/platform/soc.0/f9824900.sdhci/by-name/metadata";
 
-# init.rc
+# init.angler.rc
 insert_line init.angler.rc "init.sz.rc" after "import init.angler.sensorhub.rc" "import init.sz.rc";
-insert_line init.angler.rc "init.spectrum.rc" after "import init.angler.sensorhub.rc" "import init.spectrum.rc";
-insert_line init.angler.rc "init.spectrum.sh" after "import init.angler.sensorhub.rc" "import init.spectrum.sh";
+insert_line init.angler.rc "performance_profiles" after "import init.angler.sensorhub.rc" "import init.performance_profiles.rc";
 replace_string init.angler.rc "#    verity_load_state" "    verity_load_state" "#    verity_load_state";
 replace_string init.angler.rc "#    verity_update_state" "    verity_update_state" "#    verity_update_state";
 replace_section init.angler.rc "service atfwd" " " "service atfwd /system/bin/ATFWD-daemon\n    disabled\n    class late_start\n    user system\n    group system radio\n";
 
-$bin/sepolicy-inject -s init -t rootfs -c file -p execute_no_trans -P sepolicy;
+# sepolicy
+$bin/magiskpolicy --load sepolicy --save sepolicy \
+  "allow init rootfs file execute_no_trans" \
+;
 
 # end ramdisk changes
 
 write_boot;
 
 ## end install
-
